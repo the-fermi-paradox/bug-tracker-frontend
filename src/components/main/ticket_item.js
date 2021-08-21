e393e27d3b38375e3bf81162b2c24b24c87c6549
JG <joshgundy55@gmail.com>
2021-08-20 19:05:46 -0500

    Add due dates

--- src/components/main/ticket_item.js
+++ src/components/main/ticket_item.js
@@ -1,6 +1,6 @@
 import TicketStatus from "./ticket_status.js";
 const TicketItem = ({ ticket }) => {
-  const { title, state, date_created, reporter_id } = ticket;
+  const { title, state, date_created, due_date, reporter_id } = ticket;
   return (
     <tr className="ticket">
       <td className="ticket__text ticket__text--title">{title}</td>
@@ -9,7 +9,7 @@ const TicketItem = ({ ticket }) => {
         {new Date(date_created).toLocaleDateString("en-us")}
       </td>
       <td className="ticket__text ticket__text--date_due">
-        {new Date(date_created).toLocaleDateString("en-us")}
+        {new Date(due_date).toLocaleDateString("en-us")}
       </td>
       <td className="ticket__text ticket__text--reporter_id">{reporter_id}</td>
     </tr>
